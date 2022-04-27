4376ae48967b9ea19b96a6d659d73ede7731e4a5
Davi Serrano <daviscardoso2901@gmail.com>
2022-04-27 09:42:49 -0300

    useCase turnUserAdminController

--- src/modules/users/useCases/turnUserAdmin/TurnUserAdminController.ts
+++ src/modules/users/useCases/turnUserAdmin/TurnUserAdminController.ts
@@ -3,10 +3,14 @@ import { Request, Response } from "express";
 import { TurnUserAdminUseCase } from "./TurnUserAdminUseCase";
 
 class TurnUserAdminController {
-  constructor(private turnUserAdminUseCase: TurnUserAdminUseCase) {}
+  constructor(private turnUserAdminUseCase: TurnUserAdminUseCase) {};
 
   handle(request: Request, response: Response): Response {
-    // Complete aqui
+    const { name, email }  = request.body;
+
+    this.turnUserAdminUseCase.execute({ name, email });
+
+    return response.status(200).send();
   }
 }
 
